24fdfde76504f49ea244404c4d41ee5c67ed2a61
Mohammed Medlej <mohmedlej@gmail.com>
2023-10-09 14:05:21 +0300

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={"nav"} bg={"coral"}>
+          NAV
+        </GridItem>
+        <Show above="lg">
+          <GridItem area={"aside"} bg={"gold"}>
+            ASIDE
+          </GridItem>
+        </Show>
+        <GridItem area={"main"} bg={"dodgerblue"}>
+          MAIN
+        </GridItem>
+      </Grid>
     </>
   );
 }
