57d5e1a04c434f1aa2ef818de1b18a72e0a0991f
Mohammed Medlej <mohmedlej@gmail.com>
2023-10-09 13:32:51 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center">Hello World!</h1>
+      <p className="text-center">Empty React template.</p>
+    </>
+  );
+}
+
+export default App;
