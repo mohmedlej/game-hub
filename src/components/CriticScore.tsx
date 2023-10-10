0fd2fc4f4ddce26831a1e016484cbebd7c05e723
Mohammed Medlej <mohmedlej@gmail.com>
2023-10-10 14:03:59 +0300

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,12 @@
+import { Badge } from "@chakra-ui/react";
+
+const CriticScore = ({ score }: { score: number }) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge fontSize={14} paddingX={2} borderRadius={5} colorScheme={color}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
