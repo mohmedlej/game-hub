ad057f1235c0f65be346250b715f7672911cb22c
Mohammed Medlej <mohmedlej@gmail.com>
2023-10-11 11:02:03 +0300

    Fetch the genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,9 +5,8 @@ import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
-  const skeletons = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ];
+  const skeletons: number[] = Array.from({ length: 20 }, (_, index) => index);
+
   return (
     <>
       {error && <Text>{error}</Text>}
